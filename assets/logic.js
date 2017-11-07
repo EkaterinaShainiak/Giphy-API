39fd34da4474217829f25294abf95012dd9365ae
Ekaterina Shainiak <ekaterina.shainiak@gmail.com>
2017-11-07 15:28:14 -0800

    added buttons and gifts from initial array

--- assets/logic.js
+++ assets/logic.js
@@ -1,24 +1,46 @@
+
+$(document).ready(function(){
 var topics = ["madonna", "music", "cat"];
 console.log(topics);
 
+function createButton () {
+    
+    for (var i = 0; i < topics.length; i++) {
+        var newButton = $("<button>");
+        newButton.attr("class","seach_buttons");
+        newButton.attr("value",topics[i]);
+        newButton.text(topics[i]);
 
-for (i = 0; i < topics.length; i++) {
-    console.log("test")
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-    $.ajax({
-        url: queryURL,
-        method : "GET",
-    }).
-    done(function(response){
-        for (j = 0; j < response.data.length; j++) {
-        var topicDiv = $("div");
-        var p = $('<p>').text("Rating: " + response.data[j].rating );
-        var topicImage = $("<img>");
-        topicImage.attr("src", response.data[j].images.fixed_height_still.url);
-        topicDiv.append(p);
-        topicDiv.append(topicImage);
+        $("#buttons").prepend(newButton);
     }
 
-});
 }
+
+
+createButton ();
+$(document).on("click", ".seach_buttons", function(){
+    $("#gif").empty();  
+    console.log(this);
+    var topic = $(this).attr("value");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+    
+        $.ajax({
+            url: queryURL,
+            method : "GET",
+        }).
+        done(function(response){
+            for (var j = 0; j < response.data.length; j++) {
+            var topicDiv = $("<div>");
+            var p = $('<p>').text("Rating: " + response.data[j].rating );
+            var topicImage = $("<img>");
+            topicImage.attr("src", response.data[j].images.fixed_height_still.url);
+            topicDiv.append(p);
+            topicDiv.append(topicImage);
+            $("#gif").append(topicDiv);
+        }
+
+});
+});
+
+
+});
